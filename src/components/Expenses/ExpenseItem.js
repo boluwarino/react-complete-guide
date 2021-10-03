3b91f4363f34bf5e71ad960f2382f9f58da73651
unknown <boluwarin.oladipo@gmail.com>
2021-10-03 20:56:07 +0100

    at lecture 48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,23 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    
+
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = 'Updated!';
+        console.log('Updated!');
+    }; /*best practice to not put too much logic within JSX code
+        also, best practice to have the word handler in any function that handles an event 
+        */
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>               
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
